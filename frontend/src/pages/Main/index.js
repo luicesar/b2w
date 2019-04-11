476337b1119ab8ced6969d9e99319cf9bf14b144
César <cespop@gmail.com>
2019-04-11 17:40:11 -0400

    Consultando dados pela API swapi.io

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -4,22 +4,39 @@ import "./style.css";
 import api from "../../services/api";
 
 class App extends Component {
+
+  state = {
+    planets: {},
+    id: 0
+  };
+
+  async componentDidMount() {
+    const id = Math.floor(Math.random() * 61) + 1;
+    const { data } = await api.get(`planets/${id}`);
+    this.setState({ planets: data, id: id });
+  }
+
+  handleClick = async (e) => {
+    e.preventDefault();
+
+    const id = Math.floor(Math.random() * 61) + 1;
+    const { data } = await api.get(`planets/${id}`);
+    this.setState({ planets: data, id: id });
+  }
+
   render() {
+
+    const { planets } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <p>{planets.name}</p>
+          <p>{planets.population}</p>
+          <p>{planets.climate}</p>
+          <p>{planets.terrain}</p>
+          <button onClick={this.handleClick}>PROXIMO</button>
         </header>
       </div>
     );
